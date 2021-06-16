6c28751262fe1e1629806280fe833694f3bbe1d1
ww-an <iewnasos@163.com>
2021-06-16 17:24:09 +0800

    first commit

---
+++ src/main/java/com/keepclimb/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.keepclimb.git;
+
+/**
+ * com.keepclimb.git
+ *
+ * @author : ww-an
+ * @date : 2021/06/16
+ * Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
