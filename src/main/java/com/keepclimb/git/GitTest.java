04931c77664c606cc5e163ac8f0e9b1adac1521e
Will-An <iewnasos@163.com>
2021-06-16 17:56:08 +0800

    pull2

--- src/main/java/com/keepclimb/git/GitTest.java
+++ src/main/java/com/keepclimb/git/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
         System.out.println("hot-fix222");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("pull2 test!");
     }
 
 }
